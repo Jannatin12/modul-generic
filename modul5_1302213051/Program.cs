e6e16b15f72516260febe5768a5b4e32f3d78e30
Jannatin12 <jannatinnurrohmah@gmail.com>
2023-03-20 07:16:08 +0700

    Add Class Penjumlahan with method JumlahTigaAngka

--- modul5_1302213051/Program.cs
+++ modul5_1302213051/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using modul5_1302213051;
+using System;
+using System.Collections.Generic;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Penjumlahan jum = new Penjumlahan();
+
+        jum.JumlahTigaAngka(13, 02, 21);
+    }
+}

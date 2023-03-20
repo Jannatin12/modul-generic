9403707f23bbf44f949d852d54ab375620a2de84
Jannatin12 <jannatinnurrohmah@gmail.com>
2023-03-20 08:09:47 +0700

    Update

--- modul5_1302213051/Program.cs
+++ modul5_1302213051/Program.cs
@@ -6,6 +6,14 @@ public class Program
 {
     public static void Main(string[] args)
     {
+        Console.WriteLine("Generic Method");
+        Penjumlahan<float> jum = new Penjumlahan<float>();
+        Console.WriteLine("Hasil Penjumlahan 3 angka: ");
+        jum.JumlahTigaAngka(13, 02, 21);
+        Console.WriteLine(" ");
+
+
+        Console.WriteLine("Generic Class");
         SimpleDataBase<float, DateTime> simpleDB = new SimpleDataBase<float, DateTime>();
 
         simpleDB.AddNewData(13, DateTime.UtcNow);
